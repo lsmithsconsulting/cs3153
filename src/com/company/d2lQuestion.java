20749c6eea49367f958a3d5f9af7fbb04f128746
Lenard <lenard@lsmithsconsulting.com>
2017-09-01 01:08:26 -0700

    test commit from home machine.

--- src/com/company/d2lQuestion.java
+++ src/com/company/d2lQuestion.java
@@ -9,6 +9,19 @@ public class d2lQuestion {
 
     public void outputQuestion(PrintWriter pw){
 
+        pw.println("NewQuestion,MC,");
+        pw.println("ID,CHEM110-237")
+        pw.println("Title,This is a multiple choice question,");
+        pw.println("QuestionText,This is the question text for MC1,");
+        pw.println("Points,1,");
+                Difficulty,1,
+                Image,images/MC1.jpg,
+                Option,100,This is the correct answer,,This is feedback for option 1
+        Option,0,This is incorrect answer 1,,This is feedback for option 2
+        Option,0,This is incorrect answer 2,,This is feedback for option 3
+        Option,25,This is partially correct,,This is feedback for option 4
+        Hint,This is the hint text,,,
+        Feedback,This is the feedback text,,,
     }
 }
 
